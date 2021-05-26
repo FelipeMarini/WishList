502f003e1ee48c5e3193f60be49f41dd0f8fb004
Felipe Marini <felipemarini71188@gmail.com>
2021-05-26 18:47:21 -0300

    modelagens e scripts feitos

---
+++ DataBase/scripts/WishList_DDL.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE WishList
+GO
+
+USE WishList
+GO
+
+CREATE TABLE Usuarios
+(
+	idUsuario INT PRIMARY KEY IDENTITY,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	senha VARCHAR(100) NOT NULL
+)
+GO
+
+CREATE TABLE Desejos
+(
+	idDesejo INT PRIMARY KEY IDENTITY,
+	idUsuario INT FOREIGN KEY REFERENCES Usuarios(idUsuario),
+	descricao TEXT NOT NULL
+)
