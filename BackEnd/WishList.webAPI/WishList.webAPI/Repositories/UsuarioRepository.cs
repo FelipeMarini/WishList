0d6e771b05c862b336cedc92bdf97d860de0e10c
Felipe Marini <felipemarini71188@gmail.com>
2021-05-26 23:24:39 -0300

    API WishList feita

---
+++ BackEnd/WishList.webAPI/WishList.webAPI/Repositories/UsuarioRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WishList.webAPI.Contexts;
+using WishList.webAPI.Domains;
+using WishList.webAPI.Interfaces;
+
+namespace WishList.webAPI.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        WishListContext ctx = new WishListContext();
+
+        public void AtualizarUsuario(int id, Usuario usuarioAtualizado)
+        {
+            Usuario usuarioBuscado = ctx.Usuarios.Find(id);
+
+            if (usuarioAtualizado.Email != null)
+            {
+                usuarioBuscado.Email = usuarioAtualizado.Email;
+            }
+
+            if (usuarioAtualizado.Senha != null)
+            {
+                usuarioBuscado.Senha = usuarioAtualizado.Senha;
+            }
+
+            ctx.Usuarios.Update(usuarioBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public Usuario BuscarUsuarioPorId(int id)
+        {
+            return ctx.Usuarios.FirstOrDefault(u => u.IdUsuario == id);
+        }
+
+        public void CadastrarUsuario(Usuario novoUsuario)
+        {
+            ctx.Usuarios.Add(novoUsuario);
+
+            ctx.SaveChanges();
+        }
+
+        public void DeletarUsuario(int id)
+        {
+            Usuario usuarioBuscado = ctx.Usuarios.Find(id);
+
+            ctx.Usuarios.Remove(usuarioBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Usuario> ListarUsuarios()
+        {
+            return ctx.Usuarios.ToList();
+        }
+
+    }
+}
